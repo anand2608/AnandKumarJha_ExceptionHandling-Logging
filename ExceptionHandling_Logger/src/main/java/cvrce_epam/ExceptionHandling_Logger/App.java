914998326d5bf6ecb1add5d17306847c4f7fb936
anand2608 <54166502+anand2608@users.noreply.github.com>
2020-02-27 00:44:33 +0530

    Add files via upload

---
+++ ExceptionHandling_Logger/src/main/java/cvrce_epam/ExceptionHandling_Logger/App.java
@@ -0,0 +1,13 @@
+package cvrce_epam.ExceptionHandling_Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ExceptionHandling_Logger/src/main/java/cvrce_epam/ExceptionHandling_Logger/SimpleCompoundInterest.java
@@ -0,0 +1,6 @@
+package cvrce_epam.ExceptionHandling_Logger;
+
+public interface SimpleCompoundInterest {
+
+	double calculateInterest();
+}
